d0743752b4faebc53a0163ab457072c5ed065f5e
soderlind <per@soderlind.no>
2017-03-08 16:26:52 +0100

    Initial release

---
+++ polylang-mu-subdir-fix.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ *Plugin Name: polylang mu subdir fix
+ * Version: 0.1
+ * Author: Per Soderlind
+ * Author URI: https://soderlind.no
+ */
+
+if ( ! defined( 'PLL_CACHE_HOME_URL' ) ) {
+	define( 'PLL_CACHE_HOME_URL', false );
+}
